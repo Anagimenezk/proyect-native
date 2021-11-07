be13c732fc60ddd9b336ff8c98ff6fcb4865fead
Anagimenezk <gimeneza@udesa.edu.ar>
2021-11-07 17:14:58 -0300

    estructura, dependencias instaladas

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+//import { createDrawerNavigator} from '@react-navigation/drawer'; este trae error
 
+import Home from '../screens/home';
+import Register from '../screens/register';
+import Login from '../screens/login';
+import Perfil from '../screens/profile';
+import PostForm from '../screens/postForm';
+import { auth } from '../firebase/config';
+
+const Drawer = createDrawerNavigator();
 
 
 class Menu extends Component{
   constructor(props){
     super(props);
     this.state ={
-      posteos: [],
+      
     }
   }
 
