bb560d5b2d81ec3c2ffd1490e106ad0d333a76e5
Anagimenezk <gimeneza@udesa.edu.ar>
2021-11-15 10:51:32 -0300

    profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -5,19 +5,50 @@ class Profile extends Component{
   constructor(props){
     super(props);
     this.state ={
-     
+      
     }
   }
-
   render(){
-      return(
-          <View>
-              <Text>
-                  Profile
-              </Text>
-          </View>
-      )
+    console.log(this.props.userData);
+    return(
+      <View style={styles.container}>
+          <Text style={styles.welcome}> Bienvenido: {this.props.userData.email}</Text>
+          <Text style={styles.element}> Usuario creado el: {this.props.userData.metadata.creationTime}</Text>
+          <Text style={styles.element}> Último login: {this.props.userData.metadata.lastSignInTime}</Text>
+          <TouchableOpacity style={styles.touchable} onPress={()=>this.props.logout()}>
+            <Text style={styles.touchableText}>Logout</Text>
+          </TouchableOpacity>         
+      </View>       
+    )
   }
 }
 
-  export default Profile;
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 20,
+        marginHorizontal:10
+    },
+    welcome:{
+        fontSize:18,
+        marginTop:20,
+        marginBottom:30,
+        fontWeight: 'bold'
+    },
+    element:{
+        marginBottom:10,
+    },
+    touchable:{
+        padding: 10,
+        backgroundColor: '#dc3545',
+        marginTop: 30,
+        borderRadius: 4,
+    },
+    touchableText:{
+        fontWeight: 'bold',
+        color:'#fff',
+        textAlign: 'center'
+    }
+    
+});
+
+export default Profile;
