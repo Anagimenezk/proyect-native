9d2218879b9f108a3f737337ab526b7f1b80c1ce
Anagimenezk <gimeneza@udesa.edu.ar>
2021-11-18 10:26:42 -0300

    arreglando problemas

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,16 @@
 import app from 'firebase/app';
 import firebase from 'firebase';
 
-// Your web app's Firebase configuration
+//la constante la copio de la config de firebase en internet
 const firebaseConfig = {
-  apiKey: "AIzaSyBeUcf-Yql9jmoxvjhkXHCp7xmHCpSTT2k",
-  authDomain: "proyectoreactnative-b3c66.firebaseapp.com",
-  projectId: "proyectoreactnative-b3c66",
-  storageBucket: "proyectoreactnative-b3c66.appspot.com",
-  messagingSenderId: "382278071988",
-  appId: "1:382278071988:web:f883b9f348dc51a51349e1",
-  measurementId: "G-F4LMMJP8ET"
-};
+    apiKey: "AIzaSyBeUcf-Yql9jmoxvjhkXHCp7xmHCpSTT2k",
+    authDomain: "proyectoreactnative-b3c66.firebaseapp.com",
+    projectId: "proyectoreactnative-b3c66",
+    storageBucket: "proyectoreactnative-b3c66.appspot.com",
+    messagingSenderId: "382278071988",
+    appId: "1:382278071988:web:f883b9f348dc51a51349e1",
+    measurementId: "G-F4LMMJP8ET"
+  };
 
 app.initializeApp(firebaseConfig);
 
